da5e8d5314ca6759b1f5132fea0b25960a997392
Charlie <charlie.hu@cardworks.com>
2020-10-23 10:35:22 -0400

    Add CustomerApiController

---
+++ CWS.SimpleBank/Controllers/API/CustomerApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CWS.SimpleBank.Controllers.API
+{
+    public class CustomerApiController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            BankService service = new BankService();
+            return Ok(service.Customers());
+        }
+    }
+}
